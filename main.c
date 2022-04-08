529962cd999e706d4ce2f18179b526118d856961
Numan Bashir <sportybashir@gmail.com>
2022-04-08 11:48:48 +0200

    message

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main() {
 
+    char chr[2];
+
     printf("LAST Command: \n");
+    printf("Message: \n");
+    printf("INPUT > \t");
+    scanf("%s", &chr);
+
+    printf("You entered %s", chr);
 
 
     return 0;
