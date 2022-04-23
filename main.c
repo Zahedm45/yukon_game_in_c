cdcaba302c4924b4e26909f5454ce3f3d0b71314
Zahed <zahedm45@gmail.com>
2022-04-23 19:17:42 +0200

    Working for one block

--- main.c
+++ main.c
@@ -9,18 +9,18 @@ int main() {
 /*    initialize_card_deck();
     print_out();*/
 
-    starting_point();
+    //starting_point();
     initialize_card_deck();
 
-    //print_out();
     game_board = initialize_game_board(card_deck);
-    set_foundation(&game_board, 1, "H2");
+/*    set_foundation(&game_board, 1, "H2");
     set_foundation(&game_board, 4, "H1");
 
-    display_card_deck(game_board);
+    display_card_deck(game_board);*/
 
-    //free_game_board(&game_board);
-    //display_card_deck(game_board);
+
+    save_game_board(game_board);
+
     return 0;
 }
 
