7444a79e7e412c4e948fa9155d9d6f050324eb67
numanbashir <sportybashir@gmail.com>
2022-04-29 16:12:06 +0200

    Main game loop

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utility.c"
+#include <string.h>
 
 Game_board *game_board;
 
 int main() {
 
+
+    char input[2];
+    int length = 2;
+
+    // Initialize game board
     starting_point();
     initialize_card_deck(1);
     game_board = initialize_game_board(card_deck);
     display_game_board(game_board);
     save_game_board(game_board);
+
+    // Main game loop
+    while(strcmp(input, "stop") != 0) {
+
+
+        printf("LAST Command: \n");
+        printf("Message: \n");
+        printf("INPUT > \t");
+        scanf("%s", &input);
+
+        printf("You entered %*.*s\n", length, length, input);
+    }
+
+
 
     return 0;
 }
